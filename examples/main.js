a31649ddf086782e07c13574d20fb6911172e9d5
jikeliuyang <18817843920@163.com>
2018-11-09 14:54:33 +0800

    add hour

--- examples/main.js
+++ examples/main.js
@@ -5,7 +5,7 @@ import App from './App'
 import demoBlock from './components/demo-block.vue'
 import router from './router'
 import JKUI from '../packages/index'
-import '../packages/theme-default/lib/index.scss'
+import '../packages/theme-default/lib/index.css'
 
 Vue.component('demo-block', demoBlock)
 
